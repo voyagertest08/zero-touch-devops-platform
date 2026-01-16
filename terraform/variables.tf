287aa4b19245b76690fa892c58276d66e51a527a
chetantalele <chetantalele06@gmail.com>
2026-01-16 19:10:46 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,15 @@
-variable "app_name" {}
-variable "domain_name" {}
-variable "hosted_zone_id" {}
+variable "app_name" {
+  type = string
+}
+
+variable "domain_name" {
+  type = string
+}
+
+variable "hosted_zone_id" {
+  type = string
+}
+
+variable "cluster_name" {
+  default = "zero-touch-cluster"
+}
