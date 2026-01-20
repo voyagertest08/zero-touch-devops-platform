49a8ff37402c0acedbd769c5107d3aa0d10a863a
chetantalele <chetantalele06@gmail.com>
2026-01-20 18:38:36 +0530

    test

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "expense-tracker-terraform-state-devops"
+    bucket         = "expense-tracker-terraform-state-devops-test"
     key            = "terraform.tfstate"
     region         = "ap-south-1"
 
